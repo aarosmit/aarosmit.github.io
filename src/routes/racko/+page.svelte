84c778069d3268705d6f205b6e2acd3e1f3ec832
aarosmit <aaron@aarosmit.com>
2026-04-26 16:17:52 -0400

    racko updates

--- src/routes/racko/+page.svelte
+++ src/routes/racko/+page.svelte
@@ -15,20 +15,34 @@ let records;
 let games;
 let timeFirstHand;
 let dateFirstHand;
-let numberInARowArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let numberInARowArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 let array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+let bestLatestHand;
 
 async function getRecords () {
     records = await pb.collection('racko').getFullList({
-        sort: 'created',
+        sort: '-created',
     });
-    timeFirstHand = new Date(records[0].created).toLocaleTimeString()
-    dateFirstHand = new Date(records[0].created).toLocaleDateString()
+    timeFirstHand = new Date(records[records.length - 1].created).toLocaleTimeString()
+    dateFirstHand = new Date(records[records.length - 1].created).toLocaleDateString()
     console.log(records, timeFirstHand)
     for (let i = 0; i < records.length; i++) {
         array[records[i].numberInARow]++;
     }
-    console.log(array)
+    for (let i = 15; i > -1; i--) {
+        if (array[i] > 0) {
+            for (let j = 0; j < records.length; j++) {
+                if (i === records[j].numberInARow) {
+                    bestLatestHand = records[j]
+                    console.log(bestLatestHand)
+                    break;
+                }
+            }
+            break;
+        }
+
+    }
+
     return records;
 }
 
@@ -41,6 +55,10 @@ async function getRecords () {
 {:then records} 
 
 <p>I have dealt {records.length} hands of Racko. I started dealing Racko hands on {dateFirstHand} at {timeFirstHand}.</p>
+
+<p>I dealt the best hand so far at {new Date(bestLatestHand.created).toLocaleTimeString()} on {new Date(bestLatestHand.created).toLocaleDateString()}.</p>
+
+<p>This is the hand: {bestLatestHand.rack}</p>
 
 <table style="font-family: monospace;text-align: center;margin-left: auto;margin-right: auto;font-size: 0.9rem;width:50%;">
     <thead>
